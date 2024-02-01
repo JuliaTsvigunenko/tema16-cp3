c3bf689083583a2fc2c10f8e322f24033c1e558e
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-02-01 18:04:30 +0200

    Add files via upload

---
+++ tema 16-cp3/ConsoleApp1/DateClass.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DateClass
+    {
+
+        //Задание 1.
+
+
+        DateTime data;
+
+        /// <summary>
+        /// конструктор без параметров;
+        /// </summary>
+        public DateClass()
+        {
+
+        }
+
+        /// <summary>
+        /// конструктор, позволяющий установить: заданную дату(конструктор с тремя параметрами – год, месяц, день);
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="mounth"></param>
+        /// <param name="day"></param>
+        public DateClass(int year, int mounth, int day)
+        {
+            data = new DateTime(year, mounth, day);
+
+        }
+
+        public DateTime Data
+        { get { return data; } set { data = value; } }
+
+
+
+        //      ПЕРВЫЙ СПОСОБ РЕШЕНИЯ ЗАДАЧИ
+
+        public void DateOfThePreviousDay()
+        {
+            Console.WriteLine($"Дата предыдущего дня: { DateTime.Now.AddDays(-1)}");
+        }
+
+
+        public void NextDay()
+        {
+            Console.WriteLine($"Дата следующего дня: { DateTime.Now.AddDays(+1)}");
+        }
+
+
+        public void DaysToTheEndOfTheMonth()
+        {
+            DateTime todaydate = DateTime.Now;
+
+            DateTime firstDayOfNextMonth = new DateTime(todaydate.Year, todaydate.Month, 1).AddMonths(1);
+
+            int daysRemaining = (firstDayOfNextMonth - todaydate).Days;
+
+            Console.WriteLine($"До конца месяца осталось: {daysRemaining}");
+        }
+
+
+
+
+        //          ВТОРОЙ СПОСОБ РЕШЕНИЯ ЗАДАЧИ
+
+        //public DateTime Yesterday()
+        //{
+        //    return data.AddDays(-1);
+        //}
+
+        //public DateTime Tomorrow()
+        //{
+        //    return data.AddDays(1);
+        //}
+
+        //public int Mounth()
+        //{
+        //    return DateTime.DaysInMonth(data.Year.data.Month) - data.Day;
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ tema 16-cp3/ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задача1
+
+            //Console.Write("Year ");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Mounth ");
+            //int mounth = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Day ");
+            //int day = Convert.ToInt32(Console.ReadLine());
+
+            //DateClass dateClass = new DateClass(year, mounth, day);
+
+            ////для первого способа
+            //dateClass.DateOfThePreviousDay();
+            //dateClass.NextDay();
+            //dateClass.DaysToTheEndOfTheMonth();
+
+
+
+            //для второго способа
+            //dateClass.Yesterday();
+            //dateClass.Tomorrow();
+            //dateClass.Mounth();
+
+
+
+
+            //задача2
+
+            //сначала вводим 1ую дату
+            //Console.WriteLine("Введите первую дату: ");
+            //DateTime oneDate = DateTime.Parse(Console.ReadLine());
+            
+            ////потом вводим 2ую дату
+            //Console.WriteLine("Введите вторую дату: ");
+            //DateTime twoDate = DateTime.Parse(Console.ReadLine());
+
+            //// Мы высчитываем разницу (Difference)
+            //// TimeSpan используется для представления временной разницы между этими датами. Когда мы вычитаем одну дату из другой
+            //TimeSpan timeDifference = oneDate - twoDate;
+
+            //Console.WriteLine($"Количество дней между датами: {timeDifference.Days}");
+
+
+
+
+            //задача3
+
+
+            Console.Write("Введите время первой процедуры: ");
+            DateTime beginTime1 = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Введите время второй процедуры процедуры: ");
+            DateTime beginTime2 = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Введите общее количество процедур: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Расписание процедур: ");
+            for (int i=0; i<a; i++)
+            {
+                DateTime currentDate = beginTime1.AddMinutes(i * (beginTime2 - beginTime1).TotalMinutes);
+                Console.WriteLine($"Расписание процедур {i + 1}: {currentDate.ToShortTimeString()}");
+            }
+
+            //currentDate - текущая дата
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
